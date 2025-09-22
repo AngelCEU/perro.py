b79b3961ebd082058531b675393b9a1d083448bb
AngelCEU <angel.mateospereda@usp.ceu.es>
2025-09-22 13:56:15 +0000

    Agregar clase perro con métodos básicos y un bloque principal para instanciar un objeto

---
+++ perro.py
@@ -0,0 +1,33 @@
+class perro: 
+    def __init__ (self, color, color_ojos, altura, longitud, peso):
+        self.color = color
+        self.color_ojos = color_ojos
+        self.altura = altura
+        self.longitud = longitud
+        self.peso = peso
+
+    def sentarse(self):
+        return "El perro se ha sentado"
+    def tumbarse(self):
+        return "El perro se ha tumbado"
+    def pata(self):
+        return "El perro ha dado la pata"
+    def venir(self):
+        return "El perro ha venido"
+    
+if __name__ == "__main__":
+    bobby = perro(marrón, avellana, 50, 80, 20):
+    print(bobby.sentarse())
+
+
+
+
+        
+
+  
+
+
+
+
+    
+        
